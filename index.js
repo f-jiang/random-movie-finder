c6e3ccab494908e5a3bff6a3a32872b413aaab0b
Feilan Jiang <f-jiang@github.com>
2016-08-14 22:25:04 -0400

    Display movie genres

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 var apiKey = process.env.API_KEY;
 var config = JSON.parse(fs.readFileSync('config.json'));
 var lang = JSON.parse(fs.readFileSync('lang.json'));
+var genres = JSON.parse(fs.readFileSync('genres.json'));
 
 app.use(express.static('view'));
 
@@ -19,12 +20,7 @@ app.get('/lang', function(req, res) {
 });
 
 app.get('/genres', function(req, res) {
-    request.get({
-        url: 'http://api.themoviedb.org/3/genre/movie/list',
-        qs: {api_key: apiKey}
-    }, function(error, response, body) {
-        res.json(JSON.parse(body).genres);
-    });
+    res.json(genres);
 });
 
 app.get('/random-movie', function(req, res) {
@@ -44,6 +40,18 @@ app.get('/random-movie', function(req, res) {
 
         // convert ISO 639-1 code to language name
         movie.original_language = lang[movie.original_language].name;
+
+        // convert ISO 639-1 code to genre name
+        var ids = movie.genre_ids;
+        movie.genre_ids = '';
+        for (var i = 0; i < ids.length; i++) {
+            for (var j = 0; j < genres.length; j++) {
+                if (ids[i] === genres[j].id) {
+                    movie.genre_ids += genres[j].name + ', ';
+                }
+            }
+        }
+        movie.genre_ids = movie.genre_ids.slice(0, -2);
 
         res.json(movie);
     });
